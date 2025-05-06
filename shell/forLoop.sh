f9255117e2131a2fbcc901eec38683219614637e
Chinnipalli Shiva <chinnipallishiva26@gmail.com>
2025-05-06 20:13:50 +0530

    shell for loop

---
+++ shell/forLoop.sh
@@ -0,0 +1,17 @@
+for j in 1 2 3 4 5
+do echo "Number: $j"
+done
+
+
+for i in chinni palli shiva
+do echo "Name: $i"
+done
+
+
+for k in {1..10}
+do echo "Number: $k"
+done
+
+
+# Break – to stop loop
+# Continue – to stop current iteration and start next iteration
